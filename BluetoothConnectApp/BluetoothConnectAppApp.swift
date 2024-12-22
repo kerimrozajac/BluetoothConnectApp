8251cd2283e0f0768a2ec23996aa75d997221f18
kerimrozajac <130725852+kerimrozajac@users.noreply.github.com>
2024-12-22 01:08:16 +0100

    Initial Commit

---
+++ BluetoothConnectApp/BluetoothConnectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothConnectAppApp.swift
+//  BluetoothConnectApp
+//
+//  Created by Kerim Rozajac on 22. 12. 2024..
+//
+
+import SwiftUI
+
+@main
+struct BluetoothConnectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
